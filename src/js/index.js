9563e0282f5d96061b427747262961d505be50cb
Haiyang Hong <hong961127@gmail.com>
2020-11-21 22:00:14 -0800

    Add stagger elements list and text blinder effect

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,13 @@
-import { gsap, Power2 } from "gsap";
+import { gsap } from "gsap";
+import CSSRulePlugin from "gsap/CSSRulePlugin";
+
 import "core-js";
 import "regenerator-runtime";
 
 import "../sass/main.scss";
 import fullSizeSrc from "../img/long-cover.jpg";
 
+// Inject Date
 const int = new Intl.DateTimeFormat("en-US", {
   day: "numeric",
   month: "short",
@@ -14,7 +17,6 @@ const int = new Intl.DateTimeFormat("en-US", {
   second: "numeric",
 });
 
-// Inject Date
 setInterval(() => {
   int.format(new Date());
   coverDate.textContent = int.format(new Date());
@@ -25,13 +27,33 @@ const coverDate = document.querySelector(".date");
 const navButton = document.querySelector(".nav__links__item");
 const navOpen = document.querySelector(".nav--open");
 const coverImg = document.querySelector(".cover__image");
-const navImages = document.querySelectorAll(".nav__images > img");
 
 // Swap small image to large image
 coverImg.setAttribute("src", fullSizeSrc);
 coverImg.addEventListener("load", function (evt) {
-  console.log(evt.target.getAttribute("src"));
   evt.target.classList.remove("cover__image--blur");
+});
+
+// Selecting pseudo elements needs registing a CSSRulePlugin
+gsap.registerPlugin(CSSRulePlugin);
+const rule = CSSRulePlugin.getRule(".nav--closed h3::after");
+
+// Text reveal aniamtion
+gsap.to(rule, {
+  // Put css properties inside `cssRule key`
+  cssRule: { scaleY: 0 },
+  duration: 1,
+  delay: 0.75,
+});
+
+// Stagger animation nav links
+gsap.from(".nav--closed li", {
+  x: 75,
+  opacity: 0,
+  stagger: 0.1,
+  duration: 0.5,
+  ease: "Power2.easeOut",
+  delay: 0.5,
 });
 
 // Pause when first load
@@ -42,14 +64,14 @@ tl.to(".cover", {
   width: "60%",
   duration: 1,
   filter: "blur(1.5px)",
-  ease: Power2.easeOut,
+  ease: "Power2.easeOut",
 })
   .to(
     ".date",
     {
       y: 50,
       opacity: 0,
-      ease: Power2.easeOut,
+      ease: "Power2.easeOut",
       duration: 0.5,
     },
     "-=1"
@@ -58,7 +80,7 @@ tl.to(".cover", {
     ".nav",
     {
       height: "100%",
-      ease: Power2.easeOut,
+      ease: "Power2.easeOut",
       duration: 1,
     },
     "-=0.5" // Timing offset
@@ -73,7 +95,7 @@ tl.to(".cover", {
     {
       opacity: 1,
       x: 0,
-      ease: Power2.easeOut,
+      ease: "Power2.easeOut",
       duration: 1,
 
       // Called after current transition complete
@@ -82,6 +104,17 @@ tl.to(".cover", {
       },
     },
     "-=0.25"
+  )
+  .from(
+    ".nav__clothing li",
+    {
+      opacity: 0,
+      y: 75,
+      duration: 0.5,
+      ease: "Power2.easeOut",
+      stagger: 0.1, // Create stagger effect on list of elements
+    },
+    "-=0.75"
   );
 
 navButton.addEventListener("click", function () {
